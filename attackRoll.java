ab9335bfd82d56d543d9c29390342f337728b45c
jewettkt <49500903+jewettkt@users.noreply.github.com>
2019-05-01 19:12:19 -0400

    entered beginning code

---
+++ attackRoll.java
@@ -0,0 +1,9 @@
+public class attackRoll{
+    public static void main(Stringp[]args){
+        diceTwenty rollOnce = new diceTwenty( );
+int greataxeMod = 7;
+    int strengthMod = 4;
+    int attackRoll = rollOnce + greataxeMod + strengthMod;
+    System.out.println("Kansif's attack roll was a " + attackRoll);
+    }
+}
