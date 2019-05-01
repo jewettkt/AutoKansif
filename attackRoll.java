508ac5d4bfdd1c720f03b8dadf1373ccbe1f6f60
jewettkt <49500903+jewettkt@users.noreply.github.com>
2019-05-01 19:58:09 -0400

    Fixing misunderstanding of classes

--- attackRoll.java
+++ attackRoll.java
@@ -1,3 +1,8 @@
+import java.util.Random;
+public class diceTwenty {
+    Random rand = new Random ( );
+    int rollOnce = rand.nextInt(20) + 1;
+}
 public class attackRoll{
     public static void main(Stringp[]args){
         diceTwenty rollOnce = new diceTwenty( );
